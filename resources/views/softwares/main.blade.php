92cc659fe8599070d5de0aa2b71fe98720bd6305
Mohammad Mehdi Balouchi <mmehdibalouchi@gmail.com>
2018-06-18 22:56:57 +0430

    add cdap in softwares view

--- resources/views/softwares/main.blade.php
+++ resources/views/softwares/main.blade.php
@@ -11,6 +11,7 @@
         <div class="jumbotron" style="width: 100%;">
             <h2>Softwares</h2>
             <p></p><p></p><p></p>
+            <a href="/softwares/cdap"> <li>Complex Detection Analyzer Package (cdap)</li></a>
             <a href="/softwares/cdap"> <li>IMHRC</li></a>
             <a href="/softwares/dmn"> <li>Decomposition of metabolic networks</li></a>
         </div>
