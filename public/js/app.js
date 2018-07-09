d02d8b308901585be850014387101aa274425d19
Mohammad Mehdi Balouchi <mmehdibalouchi@gmail.com>
2018-07-09 13:43:20 +0430

    add defaults

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,5 @@
 $.backstretch("/images/banners/0.jpg");
+changeAlgorithmParametersDefaults();
 $(document).ready(function() {
     $('.js-example-basic-single').select2();
 });
@@ -25,21 +26,40 @@ function changeInputDisable(inputId, checkElementId) {
     else
         tr.disabled = true;
 }
-changeAlgorithmParametersDefaults();
-function changeAlgorithmParametersDefaults(dataset) {
-    var defaults = {
-        'AP' :['0.6'],
-        'CFinder' :['4', '0.0', '1', '0.2'],
-        'CMC' :['0.7', '0.5', '0', '3'],
-        'MCL' :['4.7'],
-        'ClusterONE' :['9', '10', '3', '20', '15', '100', '1'],
-        'RRW' :['0.6', '0.1', '0.6'],
-        'IMHRC' :['3', '1000', '0.014', '0.048', '0.78', '1', '1', '0.2']
-    };
-    jQuery.each(defaults, function(i, val) {
-        console.log(i, val);
-    });
+function changeAlgorithmParametersDefaults() {
+    var dataset = $("input[type='radio'][name='dataset']:checked")[0].value;
+    var goldstandard = $("input[type='radio'][name='goldstandard']:checked")[0].value;
 
+    var inputNames = {
+        'AP': ['Preference(P)'],
+        'CFinder': ['k-clique size(k)', 'Lower link weight threshold(w)', 'upper link weight threshold(W)', 'Maximum time of clique searching(t)'],
+        'CMC': ['Overlap threshold(w)', 'Merge threshold(m)', 'Minimum degree ratio(c)', 'Minimum size of clusters(s)'],
+        'MCL': ['Inflation(I)'],
+        'RNSC': ['Shuffling diversification length(d)', 'Diversification frequency(D)', 'Number of experiments(e)', 'Naive stopping tolerance(n)', 'Scaled stopping tolerance(N)', 'Tabu length(t)', 'Tabu tolerance(T)'],
+        'RRW': ['Restart probability(r)', 'Overlap threshold(overlap)', 'Early cutoff(lambda)'],
+        'IMHRC': ['Minimum size of cluster(min-size)', 'Maximum size of cluster(max-size)', 'Hub retrieving threshold(black-list)(γ)', 'Hub removing threshold (black-list)(β)', 'Overlap threshold(max-overlap)', 'Growing penalty(growth-penalty)', 'Hub retrieving penalty(back-penalty)', 'Minimum Density(min-density)']
+    };
+    var defaults = {
+        'mips_3_100': {
+            'collins2007': [-0.9 , 3, 0.0, 1, 0.2, 0.7, 0.5, 1, 3, 4.9, 5, 50, 3 , 10, 5, 100, 1, 0.5, 0.2, 0.5, 3, 10000, 0.002, 0.005, 0.5, 1.2, 1.0, 0.5],
+            'gavin2006_socioaffinities_rescaled': [-0.15 , 4, 0, 1, 0.2, 0.7, 0.5, 1, 3, 3.2, 9, 20, 3 , 20, 5, 100, 5, 0.6, 0.1, 0.6, 3, 10000, 0.005, 0.007, 0.8, 2.2, 1.4, 0.4],
+            'krogan2006_core': [0.35 , 3, 0, 1, 0.2, 0.7, 0.4, 1, 3, 2.3, 9, 20, 3 , 10, 5, 10, 3, 0.5, 0.2, 0.6, 3, 10000, 0.005, 0.005, 0.85, 1.2, 3.0, 0.4],
+            'krogan2006_extended': [0.4 , 3, 0, 1, 0.2, 0.7, 0.5, 1, 3, 2.3, 9, 20, 3 , 20, 1, 50, 1, 0.5, 0.2, 0.7, 3, 10000, 0.0, 0.001, 0.8, 2.4, 1.0, 0.6],
+        },
+        'sgd':{
+            'collins2007': [0.4, 3, 0.0, 1, 0.2, 0.7, 0.5, 1, 3, 4.6, 9, 50, 3 , 50, 5, 100, 1, 0.5, 0.2, 0.5, 3, 10000, 0.036, 0.098, 0.59, 2.8, 1.0, 0.3],
+            'gavin2006_socioaffinities_rescaled': [-0.6, 4, 0, 1, 0.2, 0.7, 0.5, 1, 3, 4.6, 9, 10, 3 , 20, 15, 100, 1, 0.6, 0.1, 0.6, 3, 10000, 0.018, 0.046, 0.8, 1.4, 2.0, 0.4],
+            'krogan2006_core': [0.35, 3, 0, 1, 0.2, 0.7, 0.4, 1, 3, 4.6, 3, 20, 3 , 50, 5, 50, 1, 0.5, 0.2, 0.6, 3, 10000, 0.0, 0.001, 0.79, 1.5, 1.8, 0.5],
+            'krogan2006_extended': [0.3, 4, 0, 1, 0.2, 0.7, 0.3, 1, 3, 4.6, 9, 50, 10 , 50, 1, 50, 1, 0.5, 0.2, 0.7, 3, 10000, 0.007, 0.012, 0.79, 1.6, 3.0, 0.6],
+        }
+    };
+    var index = 0;
+    jQuery.each(inputNames, function(algo, params) {
+        jQuery.each(params, function(i, param) {
+            $("input[type='number'][name='" + algo + "-" + param.replace(/ /g , "%20") + "']")[0].value = defaults[goldstandard][dataset][index];
+            index++;
+        });
+    });
 }
 
 function imhrcResult() {
--- resources/views/softwares/imhrc/start.blade.php
+++ resources/views/softwares/imhrc/start.blade.php
@@ -18,25 +18,25 @@
                         <div class="col-sm-2">Datasets</div>
                     </div>
                     <div class="form-check">
-                        <input class="form-check-input" onclick="changeInputDisable('customDataset', 'customDatasetRadio')" type="radio" name="dataset" id="collins2007Radio" value="collins2007" checked>
+                        <input class="form-check-input" onclick="changeInputDisable('customDataset', 'customDatasetRadio'); changeAlgorithmParametersDefaults()" type="radio" name="dataset" id="collins2007Radio" value="collins2007" checked>
                         <label class="form-check-label" for="collins2007Radio">
                             collins2007
                         </label>
                     </div>
                     <div class="form-check">
-                        <input class="form-check-input" onclick="changeInputDisable('customDataset', 'customDatasetRadio')" type="radio" name="dataset" id="gavin2006_socioaffinities_rescaledRadio" value="gavin2006_socioaffinities_rescaled">
+                        <input class="form-check-input" onclick="changeInputDisable('customDataset', 'customDatasetRadio'); changeAlgorithmParametersDefaults()" type="radio" name="dataset" id="gavin2006_socioaffinities_rescaledRadio" value="gavin2006_socioaffinities_rescaled">
                         <label class="form-check-label" for="gavin2006_socioaffinities_rescaledRadio">
                             gavin2006_socioaffinities_rescaled
                         </label>
                     </div>
                     <div class="form-check">
-                        <input class="form-check-input" onclick="changeInputDisable('customDataset', 'customDatasetRadio')" type="radio" name="dataset" id="krogan2006_coreRadio" value="krogan2006_core">
+                        <input class="form-check-input" onclick="changeInputDisable('customDataset', 'customDatasetRadio'); changeAlgorithmParametersDefaults()" type="radio" name="dataset" id="krogan2006_coreRadio" value="krogan2006_core">
                         <label class="form-check-label" for="krogan2006_coreRadio">
                             krogan2006_core
                         </label>
                     </div>
                     <div class="form-check">
-                        <input class="form-check-input" onclick="changeInputDisable('customDataset', 'customDatasetRadio')" type="radio" name="dataset" id="krogan2006_extendedRadio" value="krogan2006_extended">
+                        <input class="form-check-input" onclick="changeInputDisable('customDataset', 'customDatasetRadio'); changeAlgorithmParametersDefaults()" type="radio" name="dataset" id="krogan2006_extendedRadio" value="krogan2006_extended">
                         <label class="form-check-label" for="krogan2006_extendedRadio">
                             krogan2006_extended
                         </label>
@@ -60,13 +60,13 @@
                         <div class="col-sm-2">Gold Standards</div>
                     </div>
                     <div class="form-check">
-                        <input class="form-check-input" onclick="changeInputDisable('customGoldstandard', 'customGoldstandardRadio')" type="radio" name="goldstandard" id="mips_3_100Radio" value="mips_3_100" checked>
+                        <input class="form-check-input" onclick="changeInputDisable('customGoldstandard', 'customGoldstandardRadio'); changeAlgorithmParametersDefaults()" type="radio" name="goldstandard" id="mips_3_100Radio" value="mips_3_100" checked>
                         <label class="form-check-label" for="mips_3_100Radio">
                             mips_3_100
                         </label>
                     </div>
                     <div class="form-check">
-                        <input class="form-check-input" onclick="changeInputDisable('customGoldstandard', 'customGoldstandardRadio')" type="radio" name="goldstandard" id="sgdRadio" value="sgd">
+                        <input class="form-check-input" onclick="changeInputDisable('customGoldstandard', 'customGoldstandardRadio'); changeAlgorithmParametersDefaults()" type="radio" name="goldstandard" id="sgdRadio" value="sgd">
                         <label class="form-check-label" for="sgdRadio">
                             sgd
                         </label>
